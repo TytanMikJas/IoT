87a4da092894fa3ee613e2d69c128a81360e63bb
Aleksander Musz <olekmusz@gmail.com>
2023-12-21 17:11:48 +0100

    Bierz tytrk

--- project/sender/work.py
+++ project/sender/work.py
@@ -13,37 +13,35 @@ pixel_off = (0, 0, 0)
 def work_setup():
     ...
 
+def restore_pixel_illumination(pixel):
+    if (active_pixels[previous_pixel]):
+        pixels[previous_pixel] = pixel_on
+    else:
+        pixels[previous_pixel] = pixel_off
+    
 
-def encoderTurnedCallback():
+def encoderTurnedCallback(channel):
     if(encoderRight == 0):
         current_pixel = (current_pixel+1)%len(pixels)
         if (current_pixel > 0):
-            previous_pixel = current_pixel - 1
-            if (active_pixels[previous_pixel]):
-                pixels[previous_pixel] = pixel_on
-            else:
-                pixels[previous_pixel] = pixel_off
+             restore_pixel_illumination( current_pixel - 1)
     else:
-       current_pixel = (current_pixel-1)%len(pixels)
+        current_pixel = (current_pixel-1)%len(pixels)
         if (current_pixel < len(pixels) ):
-            previous_pixel = current_pixel - 1
-            if (active_pixels[previous_pixel]):
-                pixels[previous_pixel] = pixel_on
-            else:
-                pixels[previous_pixel] = pixel_off
- 
+            restore_pixel_illumination( current_pixel + 1)
+
     if (active_pixels[current_pixel]):
         pixels[current_pixel] = pixel_off
     else:
-        pixels[current_pixel] = pixel_on 
+        pixels[current_pixel] = pixel_on
+    
 
-
-def greenButtonPressedCallback():
-    active_pixels[current_pixel] = 0        
+def greenButtonPressedCallback(channel):
+    active_pixels[current_pixel] != active_pixels[current_pixel]        
 
 
 
 def work():
-    GPIO.add_event_detect(greenButton, GPIO.FALLING, callback=greenButtonPressedCallback, bouncetime=200)
+    GPIO.add_event_detect(buttonGreen, GPIO.FALLING, callback=greenButtonPressedCallback, bouncetime=200)
     GPIO.add_event_detect(encoderLeft, GPIO.FALLING, callback=encoderTurnedCallback, bouncetime=200)
     
